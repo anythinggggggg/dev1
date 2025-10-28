acc455856206b43ec494a3811d09538bca508322
Piyush Kumar <piyush.23b1531212@abes.ac.in>
2025-10-28 15:33:47 +0530

    sher

---
+++ Book-npm-app/main.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const main = () => {
+  return (
+    <div>
+      <h1>Book Store</h1>
+      <App />
+      
+    </div>
+
+  )
+}
+
+export default main
--- index.js
+++ index.js
@@ -1,55 +1,16 @@
-function register(cb) {
-  setTimeout(() => {
-    console.log("Register end");
-    cb();
-  }, 4000);
-}
-
-function sendSMS(cb) {
-  setTimeout(() => {
-    console.log("Send SMS end");
-    cb();
-  }, 3000);
-}
-
-function login(cb) {
-  setTimeout(() => {
-    console.log("Login end");
-    cb();
-  }, 2000);
-}
-
-function getData(cb) {
-  setTimeout(() => {
-    console.log("Get Data end");
-    cb();
-  }, 8000);
-}
-
-function calculateData(cb) {
-  setTimeout(() => {
-    console.log("Display Data end");
-    cb();
-  }, 3000);
-}
-
-function logout(cb) {
-  setTimeout(() => {
-    console.log("Logout end");
-    cb();
-  }, 1000);
-}
-
-register(() => {
-  sendSMS(() => {
-    login(() => {
-      getData(() => {
-        calculateData(() => {
-          logout(() => {
-            console.log("All tasks completed");
-          });
-        });
-      });
-    });
-  });
+const url = "https://dummyjson.com/users";
+const pr = fetch(url);
+pr.then((function(res){
+    return res.json();
+}))
+.then((data)=>{
+  console.log('Data =',data);
+})
+.catch((err)=>{
+  console.log('Error =',err);
+})
+.finally(()=>{
+  console.log('Inside finally');
 });
+
+
